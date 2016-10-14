9c86cc5132bef290f989d260084b3c3380b17fe8
ponder2342 <ponder-github@gluttony.de>
2016-10-14 08:43:05 +0200

    added defaults and project structure

--- run.bat
+++ run.bat
@@ -1,9 +1,14 @@
 @ECHO off
 
-SET cpath=%cd%
+:: Set default location
+SET cpath=%cd%;.\classes;.\lib
 
-FOR /f "tokens=*" %%a IN (classpath) DO CALL :concat %%a
+:: Add classpath additions if present
+IF EXIST classpath (
+  FOR /f "tokens=*" %%a IN (classpath) DO CALL :concat %%a
+)
 
+:: Run it
 java -classpath %cpath% %*
 GOTO :eof
 
