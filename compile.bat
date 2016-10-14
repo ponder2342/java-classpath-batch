4434f418e6e77d4cbfc9bd5ec9022c206b8c8303
ponder2342 <ponder-github@gluttony.de>
2016-10-14 08:42:33 +0200

    added defaults and project structure

--- compile.bat
+++ compile.bat
@@ -1,12 +1,38 @@
 @ECHO off
 
-SET cpath=%cd%
+:: Set default Locations
+SET cpath=%cd%;.\lib;.\classes
+SET spath=%cd%;.\src
 
-FOR /f "tokens=*" %%a IN (classpath) DO CALL :concat %%a
+IF EXIST classdest (
+  SET /p cdest=<classdestination
+) else (
+  SET cdest=classes
+)
 
-javac -classpath %cpath% %*
+:: Add all external JARs to our classpath if there is something in the lib folder
+:: Got the first line from there: http://bit.ly/2eezcsG
+>nul 2>nul dir /a-d /s "folderName\*" && FOR /f "tokens=*" %%a IN ('dir /b /s .\lib\*.jar') DO CALL :concat_cpath %%a
+
+:: Read in additional class- and sourcepaths from file
+IF EXIST classpath (
+  FOR /f "tokens=*" %%a IN (classpath) DO CALL :concat_classpath %%a
+)
+
+:: Read in sourcepath additions if there are any.
+IF EXIST sourcepath (
+  FOR /f "tokens=*" %%a IN (sourcepath) DO CALL :concat_spath %%a
+)
+
+:: Now call the compiler with the Configuration we just gathered.
+javac -classpath %cpath% -sourcepath %spath% -d %cdest% %*
 GOTO :eof
 
-:concat
+:concat_cpath
 SET cpath=%cpath%;%1
 GOTO :eof
+
+:concat_spath
+SET spath=%spath%;%1
+GOTO :eof
+
