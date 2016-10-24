0ec852519d523feaa470245f5dfed1807d651bc6
ponder2342 <ponder-github@gluttony.de>
2016-10-24 10:05:41 +0200

    forgot to add jars to classpath

--- run.bat
+++ run.bat
@@ -8,6 +8,10 @@ IF EXIST classpath (
   FOR /f "tokens=*" %%a IN (classpath) DO CALL :concat %%a
 )
 
+:: Add all external JARs to our classpath if there is something in the lib folder
+:: Got the first line from there: http://bit.ly/2eezcsG
+>nul 2>nul dir /a-d /s "lib\*" && FOR /f "tokens=*" %%a IN ('dir /b /s .\lib\*.jar') DO CALL :concat %%a
+
 :: Run it
 java -classpath %cpath% %*
 GOTO :eof
